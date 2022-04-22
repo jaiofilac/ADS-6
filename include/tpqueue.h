20904901329a5b0ab9749f063f0ec2366b16064d
Андрей Жаворонков <96244220+jaiofilac@users.noreply.github.com>
2022-04-22 19:03:32 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,11 +1,12 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+#include <string>
 
 template<typename T, int size>
 class TPQueue {
  private:
-  T arr[size];
+  T *arr;
   int last;
  public:
   TPQueue();
@@ -19,7 +20,7 @@ struct SYM {
 };
 
 template<typename T, int size>
-TPQueue<T, size>::TPQueue() : last(-1) { }
+TPQueue<T, size>::TPQueue() : last(-1) { arr = new T[size] }
 
 template<typename T, int size>
 T TPQueue <T, size> ::pop() {
@@ -28,13 +29,12 @@ T TPQueue <T, size> ::pop() {
 
 template<typename T, int size>
 void TPQueue<T, size>::push(const T& item) {
-  if (last >= 0){
+  if (last >= 0) {
     for (int i = 0; i <= last; i++) {
       if (arr[i].prior >= item.prior) {
         if (last >= (size - 1)) {
           throw std::string("Full!");
-        }
-        else {
+        } else {
           last++;
           for (int j = last; j > i; j--) {
             arr[j] = arr[j - 1];
@@ -42,15 +42,13 @@ void TPQueue<T, size>::push(const T& item) {
           arr[i] = item;
         }
         break;
-      }
-      else if (i == last) {
+      } else if (i == last) {
         last++;
         arr[last] = item;
         break;
       }
     }
-  }
-  else {
+  } else {
     last = 0;
     arr[last] = item;
   }
